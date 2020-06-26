852642562e6be2ac83febd5d2637631c377a05c3
gaozhonglei <gaozhonglei@yuehuikang.com>
2020-06-26 15:06:21 +0800

    xiguai

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 
 docker build . -t jenkins:web
 docker save jenkins:web -o jenkins
-docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs jenkins stop
-docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs jenkins rm
+docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs docker stop
+docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs docker rm
 docker run -d --name=jenkins-web -p 49002:49002 jenkins:web
 docker rmi $(docker images -a|grep none|awk '{print $3}')
