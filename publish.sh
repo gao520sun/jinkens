dd4d62a76564a70bc4e2952d7abcfd79a94a3234
gaozhonglei <gaozhonglei@yuehuikang.com>
2020-06-26 18:59:25 +0800

    d

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 
-docker build . -t jenkens:1.0
-docker run -d --name=mycentos -p 49002:49002 jenkens:1.0 /bin/bash
+docker build . -t nginx:1.0
+docker run -d --name=mynginx1.0 -p 80:80 nginx:1.0 /bin/bash
 docker rmi $(docker images -a|grep none|awk '{print $3}')
