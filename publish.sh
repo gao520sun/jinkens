d102518ba73c7e223135b2907a71c5c4ef3cd932
gaozhonglei <gaozhonglei@yuehuikang.com>
2020-06-26 15:15:37 +0800

    a

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 
 docker build . -t jenkins:web
-docker save jenkins:web -o jenkins
+# docker save jenkins:web -o jenkins
 docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs docker stop
 docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs docker rm
 docker run -d --name=jenkins-web -p 49002:49002 jenkins:web
