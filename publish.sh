b3bc679fa6c1d35f7d8f81b6c811fb5f18ae4d8f
gaozhonglei <gaozhonglei@yuehuikang.com>
2020-06-26 14:50:05 +0800

    ss

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 
 docker build . -t jenkins:web
-docker save jenkins:web -o fengyu
-docker ps -a|grep "fengyu-web" | awk '{print $1}' | xargs jenkins stop
-docker ps -a|grep "fengyu-web" | awk '{print $1}' | xargs jenkins rm
-docker run -d --name=fengyu-web -p 49002:49002 fengyu:web
+docker save jenkins:web -o jenkins
+docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs jenkins stop
+docker ps -a|grep "jenkins-web" | awk '{print $1}' | xargs jenkins rm
+docker run -d --name=jenkins-web -p 49002:49002 jenkins:web
 docker rmi $(docker images -a|grep none|awk '{print $3}')
